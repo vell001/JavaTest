7ce3bd8032ce6929ca8707831040b4e1c4dc770d
vell001 <vell001@qq.com>
2014-01-16 11:40:19 +0800

    Add reflection test...
    Signed-off-by: vell001 <vell001@qq.com>

--- src/properties/TestProperties.java
+++ src/properties/TestProperties.java
@@ -1,12 +1,12 @@
-package properties_test;
+package properties;
 
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class TestProperties {
 	public static void main(String[] args) {
-		ResourceBundle res_zh = ResourceBundle.getBundle("properties_test/test", Locale.CHINA );
-		ResourceBundle res_en = ResourceBundle.getBundle("properties_test/test", Locale.US);
+		ResourceBundle res_zh = ResourceBundle.getBundle("properties/test", Locale.CHINA );
+		ResourceBundle res_en = ResourceBundle.getBundle("properties/test", Locale.US);
 		System.out.println(Locale.CHINA + ":" + res_zh.getString("welcome"));
 		System.out.println(Locale.US+ ":" + res_en.getString("welcome"));
 	}
